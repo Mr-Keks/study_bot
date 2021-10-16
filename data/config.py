6ee6f0ba373584baca8dce7c3a0a36f3ed6d73ef
Mr Keks <levelel744@gmail.com>
2021-10-16 18:07:41 +0300

    change dir token

--- data/config.py
+++ data/config.py
@@ -1,12 +1,10 @@
-import string
+﻿import string
 from environs import Env
 
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
 env = Env()
 env.read_env()
 
-#BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-BOT_TOKEN = "1886026763:AAEE-0KEz33QsG2Mpx-eHrubKn8vYE7v7Mg"
+BOT_TOKEN = env.str("BOT_TOKEN")
 IP = env.str("ip")
 DB_USER = env.str("DB_USER")
 DB_PASSWORD = env.str("DB_PASSWORD")
